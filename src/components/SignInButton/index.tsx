fb7af074719e0538a88adab1df4c97e31a964661
Dimas <dimas_fcn@outlook.com>
2021-04-19 21:01:03 -0300

    signin2

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,55 +1,30 @@
-import { useState } from 'react';
-import { signIn, signOut, useSession } from 'next-auth/client';
-import { CgSpinner } from 'react-icons/cg';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const [signInLoading, setSignInLoading] = useState(false);
-  const [session, loading] = useSession();
+    const [session] = useSession()
 
-  if (loading || signInLoading) {
-    return (
-      <button
-        type="button"
-        disabled={loading || signInLoading}
-        className={`${styles.signInButton} ${styles.loading}`}
-      >
-        <CgSpinner color="#eba417" className={styles.closeIcon} />
-      </button>
+    return !!session ? (
+        <button 
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button 
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417" />
+            Sign in with Github
+        </button>
     );
-  }
-
-  if (session) {
-    return (
-      <button
-        type="button"
-        className={styles.signInButton}
-        onClick={() => {
-          setSignInLoading(true);
-          signOut();
-        }}
-      >
-        <FaGithub color="#04d361" />
-        {session.user.name}
-        <FiX color="#737380" className={styles.closeIcon} />
-      </button>
-    );
-  }
-
-  return (
-    <button
-      type="button"
-      className={styles.signInButton}
-      onClick={() => {
-        setSignInLoading(true);
-        signIn('github');
-      }}
-    >
-      <FaGithub color="#eba417" />
-      Sign in with GitHub
-    </button>
-  );
 }
